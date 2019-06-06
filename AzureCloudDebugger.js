1d9565bc9bb10f86b8f90338e0bce5872def938d
Stephen Cushen <6130247+cushenst@users.noreply.github.com>
2019-06-06 14:02:50 +0100

    Reformatted time in output.

--- AzureCloudDebugger.js
+++ AzureCloudDebugger.js
@@ -35,7 +35,7 @@ app.get("/health", function (req, res) {
 
 // WebSocket server
 webSocketServer.on("connection", function connection(connection) {
-    console.log("client connected @ " + Date().toString().slice(0, 24));
+    console.log(Date().toString().slice(0, 24) + " - client connected");
 
     //handles when the client send the connection information to the server
     connection.on("message", function (message) {
@@ -78,7 +78,7 @@ webSocketServer.on("connection", function connection(connection) {
                     if (connectedToPartition === false) {
                         connection.send('{"Connected":"True"}');
                         connectedToPartition = true;
-                        console.log("Device Connected to Azure @ " + Date().toString().slice(0, 24));
+                        console.log(Date().toString().slice(0, 24) + " - Device Connected to Azure" );
                     }
 
                     //check if the message received is after the connection time of the client
@@ -100,7 +100,7 @@ webSocketServer.on("connection", function connection(connection) {
 
                         //format it in a json string
                         var responseJSON = JSON.stringify(messageData);
-                        console.log(responseJSON + " @ " + Date().toString().slice(0, 24));
+                        console.log(Date().toString().slice(0, 24) + " - " + responseJSON);
 
                         //check to see if the connection has been closed by the client
                         if (connection.readyState === 3) {
@@ -114,7 +114,7 @@ webSocketServer.on("connection", function connection(connection) {
                     }
                     // there was an error receiving a message from a partition.
                 }, error => {
-                    console.log("Error when receiving message: " + error + " @ " + Date().toString().slice(0, 24));
+                    console.log(Date().toString().slice(0, 24) + " - Error when receiving message: " + error);
                 });
             }
 
@@ -123,7 +123,7 @@ webSocketServer.on("connection", function connection(connection) {
         // there was an error connection to the eventhub
         main().catch(err => {
             //print the error
-            console.log("Error occurred: ", err, "\n@ " + Date().toString().slice(0, 24));
+            console.log(Date().toString().slice(0, 24) + " - Error occurred: ", err);
 
             //send error to the client
             connection.send(String(err));
@@ -137,5 +137,5 @@ webSocketServer.on("connection", function connection(connection) {
 const details = httpServer.listen(port, function () {
     const host = details.address().address;
     const port = details.address().port;
-    console.log("app listening at http://%s:%s", host, port, " @ " + Date().toString().slice(0, 24));
+    console.log(Date().toString().slice(0, 24) + " - app listening at http://%s:%s", host, port);
 });
